d9328b9fd6a2b905a9ad9b1c10a159d240624bee
Sami3160 <samiii@localhost.localdomain>
2024-08-31 14:16:23 +0530

    small chnages

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import Baner from "./Baner";
 import Tranding from "./Trending";
 import Category from "./Category";
-
+import Landingpage from "./Landingpage";
 function Home() {
     
     useEffect(()=>{document.title = "BitKart: Home";})
